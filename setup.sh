903ddb859784ae86944dc28e31f1b4463ab49f93
Bryce Kunz <6868100+TweekFawkes@users.noreply.github.com>
2025-05-04 05:31:48 +0000

    .

--- setup.sh
+++ setup.sh
@@ -7,4 +7,8 @@ sudo apt-get install -y python3-pip
 sudo apt-get install -y python-is-python3
 
 # Install dnsutils
-sudo apt-get install -y iputils-ping
+# sudo apt-get install -y iputils-ping
+
+# Install 7zip
+sudo apt-get install -y p7zip-full
+sudo apt-get install -y file
